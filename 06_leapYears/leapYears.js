af63a97cfbeebd6e99a4659ddd840c0181973b6a
fere1304 <ferendinot@gmail.com>
2024-01-16 17:30:50 +0200

    Implemented function that checks for leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
-};
-
-// Do not edit below this line
-module.exports = leapYears;
+function leapYears(year) {
+    if (year % 4 === 0) {
+      if (year % 100 === 0) {
+        return year % 400 === 0;
+      } else {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  }
+  
+  module.exports = leapYears;
+  
