8d5308549bbfa2cf650e12ee9397baea5e9f35a8
fere1304 <ferendinot@gmail.com>
2024-01-24 21:22:50 +0200

    Completed task by implementing the algorithm for converting Celsius to Fahrenheit and vice-versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
-};
+// tempConversion.js
 
-const convertToFahrenheit = function() {
-};
+function convertToCelsius(fahrenheit) {
+  const celsius = (fahrenheit - 32) * (5 / 9);
+  return Math.round(celsius * 10) / 10; // Round to 1 decimal place
+}
 
-// Do not edit below this line
-module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
-};
+function convertToFahrenheit(celsius) {
+  const fahrenheit = (celsius * 9 / 5) + 32;
+  return Math.round(fahrenheit * 10) / 10; // Round to 1 decimal place
+}
+
+module.exports = { convertToCelsius, convertToFahrenheit };
